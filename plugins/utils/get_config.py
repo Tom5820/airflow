2eb084a4bcc63c600c99345e94e6cda2fdb1f63f
Tom5820 <lannguyenhoang582000@gmail.com>
2025-12-11 11:54:48 +0700

    init

--- plugins/utils/get_config.py
+++ plugins/utils/get_config.py
@@ -1,5 +1,26 @@
+# plugins/utils/get_config.py
 from airflow.models import Variable
 
-CONFIG = Variable.get("SECRET_CONFIG", deserialize_json=True)
+_CONFIG = None
+
+def get_config():
+    """Lazy load config from Airflow Variables"""
+    global _CONFIG
+    if _CONFIG is None:
+        _CONFIG = Variable.get("SECRET_CONFIG", deserialize_json=True)
+    return _CONFIG
+
 def get(key: str, default=None):
-    return CONFIG.get(key, default)
+    """Get config value by key"""
+    config = get_config()
+    return config.get(key, default)
+
+# Để backward compatible, có thể dùng property
+class ConfigProxy:
+    def __getitem__(self, key):
+        return get_config()[key]
+    
+    def get(self, key, default=None):
+        return get_config().get(key, default)
+
+CONFIG = ConfigProxy()
