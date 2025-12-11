d9c62c400892cbca21db123390233d244ce85f06
Tom5820 <lannguyenhoang582000@gmail.com>
2025-12-11 13:37:37 +0700

    init

--- dags/example.py
+++ dags/example.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
-# from plugins.utils.spark_client import create_spark_job
+from plugins.utils.spark_client import create_spark_job
 
 default_args = {
     "owner": "lannh",
@@ -10,9 +10,9 @@ default_args = {
 }
 
 
-def hello_world(**context):
-    print("Hello from Airflow!")
-    return "done"
+# def hello_world(**context):
+#     print("Hello from Airflow!")
+#     return "done"
 
 with DAG(
     dag_id="spark_test_with_helper",
@@ -21,20 +21,14 @@ with DAG(
     schedule=None,
     catchup=False,
 ) as dag:
-    task_hello = PythonOperator(
-        task_id="hello_task",
-        python_callable=hello_world
+    spark_test = create_spark_job(
+        task_id="spark_test_task",
+        app_name="spark-test",
+        # Upload test_script.py lên MinIO trước
+        py_file="s3a://aiqg-spark-source/test.py",
+        arguments=["--name", "{{ ds }}"],
+        driver_memory="512m",
+        executor_memory="512m",
+        executor_instances=1
     )
-
-    task_hello
-
-    # spark_test = create_spark_job(
-    #     task_id="spark_test_task",
-    #     app_name="spark-test",
-    #     # Upload test_script.py lên MinIO trước
-    #     py_file="s3a://aiqg-spark-source/test.py",
-    #     arguments=["--name", "{{ ds }}"],
-    #     driver_memory="512m",
-    #     executor_memory="512m",
-    #     executor_instances=1
-    # )
+    spark_test
