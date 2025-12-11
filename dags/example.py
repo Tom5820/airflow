99e9f0395fe3fb6d81e379108c1ceb6717ab3189
Tom5820 <lannguyenhoang582000@gmail.com>
2025-12-11 12:29:17 +0700

    init

--- dags/example.py
+++ dags/example.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from airflow import DAG
+from airflow.operators.python import PythonOperator
 
 # from plugins.utils.spark_client import create_spark_job
 
@@ -8,6 +9,11 @@ default_args = {
     "depends_on_past": False,
 }
 
+
+def hello_world(**context):
+    print("Hello from Airflow!")
+    return "done"
+
 with DAG(
     dag_id="spark_test_with_helper",
     default_args=default_args,
@@ -15,14 +21,20 @@ with DAG(
     schedule_interval=None,
     catchup=False,
 ) as dag:
-
-    spark_test = create_spark_job(
-        task_id="spark_test_task",
-        app_name="spark-test",
-        # Upload test_script.py lên MinIO trước
-        py_file="s3a://aiqg-spark-source/test.py",
-        arguments=["--name", "{{ ds }}"],
-        driver_memory="512m",
-        executor_memory="512m",
-        executor_instances=1
+    task_hello = PythonOperator(
+        task_id="hello_task",
+        python_callable=hello_world
     )
+
+    task_hello
+
+    # spark_test = create_spark_job(
+    #     task_id="spark_test_task",
+    #     app_name="spark-test",
+    #     # Upload test_script.py lên MinIO trước
+    #     py_file="s3a://aiqg-spark-source/test.py",
+    #     arguments=["--name", "{{ ds }}"],
+    #     driver_memory="512m",
+    #     executor_memory="512m",
+    #     executor_instances=1
+    # )
