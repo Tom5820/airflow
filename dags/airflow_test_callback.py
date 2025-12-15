3329f30a2d10e9fbad772a8af4ef9bd1899aec30
Tom5820 <lannguyenhoang582000@gmail.com>
2025-12-15 16:04:06 +0700

    init

---
+++ dags/airflow_test_callback.py
@@ -0,0 +1,40 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.utils.dates import days_ago
+
+from airflow_callbacks import dag_failure_callback
+
+
+def success_task():
+    print("Task này chạy OK")
+
+
+def fail_task():
+    raise Exception("Cố tình fail để test DAG failure callback")
+
+
+with DAG(
+    dag_id="example_dag_task_fail",
+    start_date=days_ago(1),
+    schedule_interval=None,
+    catchup=False,
+    on_failure_callback=dag_failure_callback,
+    tags=["example", "failure-test"],
+) as dag:
+
+    start = EmptyOperator(task_id="start")
+
+    task_ok = PythonOperator(
+        task_id="task_ok",
+        python_callable=success_task,
+    )
+
+    task_fail = PythonOperator(
+        task_id="task_fail",
+        python_callable=fail_task,
+    )
+
+    end = EmptyOperator(task_id="end")
+
+    start >> task_ok >> task_fail >> end
