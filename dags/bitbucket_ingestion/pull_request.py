74badb0f193737abdb28e226644f315a2d87cfe4
Tom5820 <lannguyenhoang582000@gmail.com>
2026-01-28 13:55:32 +0700

    init

--- dags/bitbucket_ingestion/pull_request.py
+++ dags/bitbucket_ingestion/pull_request.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 
-from plugins.utils.bitbucket.bitbucket_export import fetch_entity_by_repo
+from plugins.utils.bitbucket.bitbucket_export import fetch_entity_by_repo, fetch_api_to_minio
 from plugins.utils.common.get_config import CONFIG
 from plugins.utils.common.spark_client import create_spark_job
 
