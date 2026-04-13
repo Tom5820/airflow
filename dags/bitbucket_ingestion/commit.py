0f1016d1561241b39ddd423b5b346bbc6259898f
MIBUKAA <163650078+MIBUKAA@users.noreply.github.com>
2026-04-13 16:48:35 +0700

    Add params to commit job configuration

--- dags/bitbucket_ingestion/commit.py
+++ dags/bitbucket_ingestion/commit.py
@@ -35,6 +35,7 @@ with DAG(
             "aws_conn_id": "minio_connection",
             "object_prefix": f"{CONFIG['bitbucket_raw_prefix_path']}/commit/date={execution_date}",
             "partition_date": execution_date,
+            "params": "pagelen=50",
         },
     )
 
